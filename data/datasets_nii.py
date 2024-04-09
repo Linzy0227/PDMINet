5df9eb737638a1806b707c0d728b810841342cca
Lyle <45514492+Linzy0227@users.noreply.github.com>
2024-04-09 20:29:47 +0800

    Update datasets_nii.py

--- data/datasets_nii.py
+++ data/datasets_nii.py
@@ -24,65 +24,6 @@ for i in range(336, 370):
 for i in range(260, 336):
     LGG.append(str(i).zfill(3))
 
-# mask_array = np.array([[True, False, False, False], [False, True, False, False], [False, False, True, False], [False, False, False, True],
-#                       [True, True, False, False], [True, False, True, False], [True, False, False, True], [False, True, True, False], [False, True, False, True], [False, False, True, True], [True, True, True, False], [True, True, False, True], [True, False, True, True], [False, True, True, True],
-#                       [True, True, True, True]])
-"""训练集8：2划分"""
-
-
-# class Brats_loadall_nii(Dataset):
-#     def __init__(self, transforms='', root=None, num_cls=4):
-#
-#         '''Lizy'''
-#         patients_dir = glob.glob(join(root, 'vol', '*_vol.npy'))
-#         patients_dir.sort(key=lambda x: x.split('/')[-1][:-8])
-#         print('###############', len(patients_dir))
-#         n_patients = len(patients_dir)
-#         pid_idx = np.arange(n_patients)
-#         np.random.seed(0)
-#         np.random.shuffle(pid_idx)
-#
-#         volpaths = []  # 训练数据集
-#         for i in enumerate(pid_idx):
-#             volpaths.append(patients_dir[i])
-#         datalist = [x.split('/')[-1].split('_vol')[0] for x in volpaths]  # 训练数据id
-#         '''Lizy'''
-#
-#         self.volpaths = volpaths
-#         self.transforms = eval(transforms or 'Identity()')
-#         self.names = datalist
-#         self.num_cls = num_cls
-#         self.modal_ind = np.array([0, 1, 2, 3])
-#
-#     def __getitem__(self, index):
-#
-#         volpath = self.volpaths[index]
-#         name = self.names[index]
-#
-#         x = np.load(volpath)
-#         segpath = volpath.replace('vol', 'seg')
-#         y = np.load(segpath)
-#         x, y = x[None, ...], y[None, ...]
-#
-#         x, y = self.transforms([x, y])  # x:[1, 128, 128, 128, 4], y:[1, 128, 128, 128]
-#
-#         x = np.ascontiguousarray(
-#             x.transpose(0, 4, 1, 2, 3))  # [Bsize,channels,Height,Width,Depth] [1, 4, 128, 128, 128]
-#         _, H, W, Z = np.shape(y)  # _, H, W, Z:1, 128, 128, 128
-#         y = np.reshape(y, (-1))  # y.shape:(2097152,)
-#         one_hot_targets = np.eye(self.num_cls)[y]  # one_hot_targets:[2097152, 4]
-#         yo = np.reshape(one_hot_targets, (1, H, W, Z, -1))  # yo:[1, 128, 128, 128, 4]
-#         yo = np.ascontiguousarray(yo.transpose(0, 4, 1, 2, 3))  # yo:[1, 4, 128, 128, 128]  标签和输入的x的输出shape一致
-#
-#         x = x[:, self.modal_ind, :, :, :]
-#
-#         x = torch.squeeze(torch.from_numpy(x), dim=0)  # [4, 128, 128, 128]
-#         yo = torch.squeeze(torch.from_numpy(yo), dim=0)  # [4, 128, 128, 128]
-#
-#         return x, yo, name
-#
-#     def __len__(self):
-#         return len(self.volpaths)
 
 class Brats_loadall_nii(Dataset):
     def __init__(self, transforms='', root=None, num_cls=4):
@@ -96,10 +37,10 @@ class Brats_loadall_nii(Dataset):
         np.random.seed(0)
         np.random.shuffle(pid_idx)
 
-        volpaths = []  # 训练数据集
+        volpaths = []
         for i in pid_idx:
             volpaths.append(patients_dir[i])
-        datalist = [x.split('/')[-1].split('_vol')[0] for x in volpaths]  # 训练数据id
+        datalist = [x.split('/')[-1].split('_vol')[0] for x in volpaths]
         '''Lizy'''
 
         self.volpaths = volpaths
@@ -124,9 +65,9 @@ class Brats_loadall_nii(Dataset):
             x.transpose(0, 4, 1, 2, 3))  # [Bsize,channels,Height,Width,Depth] [1, 4, 128, 128, 128]
         _, H, W, Z = np.shape(y)  # _, H, W, Z:1, 128, 128, 128
         y = np.reshape(y, (-1))  # y.shape:(2097152,)
-        one_hot_targets = np.eye(self.num_cls)[y]  # one_hot_targets:[2097152, 4]
-        yo = np.reshape(one_hot_targets, (1, H, W, Z, -1))  # yo:[1, 128, 128, 128, 4]
-        yo = np.ascontiguousarray(yo.transpose(0, 4, 1, 2, 3))  # yo:[1, 4, 128, 128, 128]  标签和输入的x的输出shape一致
+        one_hot_targets = np.eye(self.num_cls)[y]
+        yo = np.reshape(one_hot_targets, (1, H, W, Z, -1)) 
+        yo = np.ascontiguousarray(yo.transpose(0, 4, 1, 2, 3)) 
 
         x = x[:, self.modal_ind, :, :, :]
 
@@ -137,9 +78,6 @@ class Brats_loadall_nii(Dataset):
 
     def __len__(self):
         return len(self.volpaths)
-
-"""训练集8：2划分"""
-
 
 class Brats_loadall_test_nii(Dataset):
     def __init__(self, transforms='', root=None, modal='all', test_file='test.txt'):
@@ -160,12 +98,12 @@ class Brats_loadall_test_nii(Dataset):
         np.random.shuffle(pid_idx)
         n_fold_list = np.split(pid_idx, 5)
 
-        volpaths = []  # 测试数据集
+        volpaths = []
         for i, fold in enumerate(n_fold_list):
             if i == 0:
                 for idx in fold:
                     volpaths.append(patients_dir[idx])
-        datalist = [x.split('/')[-1].split('_vol')[0] for x in volpaths]  # 测试数据id
+        datalist = [x.split('/')[-1].split('_vol')[0] for x in volpaths]
         '''Yao'''
 
         self.volpaths = volpaths
@@ -192,7 +130,7 @@ class Brats_loadall_test_nii(Dataset):
         x, y = x[None, ...], y[None, ...]
         x, y = self.transforms([x, y])
 
-        x = np.ascontiguousarray(x.transpose(0, 4, 1, 2, 3))  # [Bsize,channels,Height,Width,Depth]
+        x = np.ascontiguousarray(x.transpose(0, 4, 1, 2, 3))
         y = np.ascontiguousarray(y)
 
         x = x[:, self.modal_ind, :, :, :]
@@ -238,7 +176,7 @@ class Brats_loadall_val_nii(Dataset):
         x, y = x[None, ...], y[None, ...]
         x, y = self.transforms([x, y])
 
-        x = np.ascontiguousarray(x.transpose(0, 4, 1, 2, 3))  # [Bsize,channels,Height,Width,Depth]
+        x = np.ascontiguousarray(x.transpose(0, 4, 1, 2, 3))
         y = np.ascontiguousarray(y)
         x = x[:, self.modal_ind, :, :, :]
 
